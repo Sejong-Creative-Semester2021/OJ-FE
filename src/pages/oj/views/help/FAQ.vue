2889b7046501c729b488b551ff204069104504a4
JYeonKim <juyeonk.kor@gmail.com>
2021-09-02 22:00:22 +0900

    faq - add oarderby, add arrow button and align right

--- src/pages/oj/views/help/FAQ.vue
+++ src/pages/oj/views/help/FAQ.vue
@@ -1,23 +1,36 @@
 <template>
-  <Panel shadow :padding="10">
+  <Panel shadow :padding="10" class="size">
     <div slot="title">
       {{$t('m.FAQ')}}
     </div>
-    <v-expansion-panels focusable>
-      <v-expansion-panel v-for="faq in faqs" :key="faq.question">
-        <v-expansion-panel-header>
-          {{faq.question}}
-        </v-expansion-panel-header>
-        <v-expansion-panel-content v-html="faq.answer" key="answer" class="content-container">
-          {{faq.answer}}
-        </v-expansion-panel-content>
-      </v-expansion-panel>
-    </v-expansion-panels>
+    <div>
+      <v-expansion-panels focusable>
+        <v-expansion-panel v-for="faq in orderedFAQ" :key="faq.question">
+          <v-expansion-panel-header>
+            <span class="header">{{faq.question}}</span>
+            <template v-slot:actions>
+              <v-icon color="primary" class="icon">
+                $expand
+              </v-icon>
+            </template>
+          </v-expansion-panel-header>
+          <v-expansion-panel-content v-html="faq.answer" key="answer" class="content-container">
+            {{faq.answer}}
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
+    </div>
   </Panel>
 </template>
 
+<script src="./js/vue.js"></script>
+<script src="./js/lodash.min.js"></script>
+
 <script>
   import api from '@oj/api'
+  import _ from 'lodash'
+  // lodash를 이용해서 orderBy를 사용하는 것이기 때문에 위에서 script를 반드시 해줘야함
+  // _(언더바)가 정의되지 않는다는 오류 발생-> lodash사용을 명시적으로 하지 않았기 때문에 생기는 에러. import를 통해서 에러 해결 가능
 
   export default {
     name: 'FAQ',
@@ -39,6 +52,14 @@
           this.faqs = res.data.data.results
         })
       }
+    },
+    computed: {
+      orderedFAQ: function () {
+        return _.orderBy(this.faqs, ['id'], ['asc'])
+        // orderBy를 created_by를 통해서 정렬하고자 했는데 실패
+        // -> 살펴보니 created_by가 단순히 숫자로만 이뤄진게 아니여서 그런듯.
+        // 결국은 아이디로 정렬
+      }
     }
   }
 </script>
@@ -46,5 +67,16 @@
 <style lang="less" scoped>
   .content-container {
     padding: 20px 20px 20px 20px;
+  }
+  .icon {
+    order:1;
+  }
+  .header{
+    order:0;
+    text-align: left;
+  }
+  .size{
+    margin-right: 15%;
+    margin-left: 15%;
   }
 </style>
